e482749398efd6d950aff45f89939f7200d02cb9
Ahmed Bello <bellahoptions@gmail.com>
2026-06-17 16:41:52 +0100

    feat: Register view responses for authentication and password management

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,13 +7,27 @@ use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Contracts\ConfirmPasswordViewResponse;
+use Laravel\Fortify\Contracts\LoginViewResponse;
+use Laravel\Fortify\Contracts\RegisterViewResponse;
+use Laravel\Fortify\Contracts\RequestPasswordResetLinkViewResponse;
+use Laravel\Fortify\Contracts\ResetPasswordViewResponse;
+use Laravel\Fortify\Contracts\TwoFactorChallengeViewResponse;
+use Laravel\Fortify\Contracts\VerifyEmailViewResponse;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Http\Responses\SimpleViewResponse;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $this->app->singleton(LoginViewResponse::class, fn() => new SimpleViewResponse('admin.login'));
+        $this->app->singleton(VerifyEmailViewResponse::class, fn() => new SimpleViewResponse('admin.verify-email'));
+        $this->app->singleton(TwoFactorChallengeViewResponse::class, fn() => new SimpleViewResponse('admin.two-factor'));
+        $this->app->singleton(RequestPasswordResetLinkViewResponse::class, fn() => new SimpleViewResponse('admin.passwords.email'));
+        $this->app->singleton(ResetPasswordViewResponse::class, fn() => new SimpleViewResponse('admin.passwords.reset'));
+        $this->app->singleton(RegisterViewResponse::class, fn() => new SimpleViewResponse('admin.login'));
+        $this->app->singleton(ConfirmPasswordViewResponse::class, fn() => new SimpleViewResponse('admin.login'));
     }
 
     public function boot(): void
